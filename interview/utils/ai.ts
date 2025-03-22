0c4484dc795cca21133ee3141735f2bd146090b1
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2025-03-22 18:59:04 -0400

    Add a prompt template

--- interview/utils/ai.ts
+++ interview/utils/ai.ts
@@ -1,7 +1,9 @@
 'use server';
 
-import { NextResponse } from 'next/server';
 import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { NextResponse } from 'next/server';
+import { v4 as uuidv4 } from "uuid";
 import {
     START,
     END,
@@ -9,7 +11,15 @@ import {
     StateGraph,
     MemorySaver,
   } from "@langchain/langgraph";
-import { v4 as uuidv4 } from "uuid";
+
+// add in a system message with some custom instructions (but still taking messages as input)
+const promptTemplate = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      "You talk like a master yoda from star wars. Answer all questions to the best of your ability.",
+    ],
+    ["placeholder", "{messages}"],
+]);
 
 // Define the function that calls the model
 const callModel = async (state: typeof MessagesAnnotation.State) => {
@@ -28,8 +38,29 @@ const workflow = new StateGraph(MessagesAnnotation)
     .addEdge(START, "model")
     .addEdge("model", END);
 
+// Define the function that calls the model
+const callModel2 = async (state: typeof MessagesAnnotation.State) => {
+    const llm = new ChatOpenAI({
+        model: "gpt-4o-mini",
+        temperature: 0
+    });
+    const prompt = await promptTemplate.invoke(state);
+    const response = await llm.invoke(prompt);
+    // Update message history with response:
+    return { messages: [response] };
+};
+
+// Define a new graph
+const workflow2 = new StateGraph(MessagesAnnotation)
+  // Define the (single) node in the graph
+  .addNode("model", callModel2)
+  .addEdge(START, "model")
+  .addEdge("model", END);
+
+  // Add memory
 const memory = new MemorySaver();
 const app = workflow.compile({ checkpointer: memory });
+const app2 = workflow2.compile({ checkpointer: new MemorySaver() });
 const config = { configurable: { thread_id: uuidv4() } };
 
 export const response = async (prompt) => {
@@ -41,7 +72,7 @@ export const response = async (prompt) => {
     ];
 
     // The output contains all messages in the state.
-    const output = await app.invoke({ messages: input }, config);
+    const output = await app2.invoke({ messages: input }, config);
     // This will log the last message in the conversation.
     return output.messages[output.messages.length - 1]
 }
