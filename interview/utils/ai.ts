3db15305afcf7a2648adc23e7d0c6f5c3aa63961
Ginger Williams <gingerwilliams@MacBook-Pro-2.local>
2025-03-22 22:28:27 -0400

    Added AiChat features https://js.langchain.com/docs/tutorials/chatbot

--- interview/utils/ai.ts
+++ interview/utils/ai.ts
@@ -10,13 +10,51 @@ import {
     MessagesAnnotation,
     StateGraph,
     MemorySaver,
-  } from "@langchain/langgraph";
+    Annotation
+} from "@langchain/langgraph";
+import {
+    SystemMessage,
+    HumanMessage,
+    AIMessage,
+    trimMessages,
+} from "@langchain/core/messages";
+
+// Define the State
+const GraphAnnotation = Annotation.Root({
+    ...MessagesAnnotation.spec,
+    language: Annotation<string>(),
+});
+
+const trimmer = trimMessages({
+    maxTokens: 10,
+    strategy: "last",
+    tokenCounter: (msgs) => msgs.length,
+    includeSystem: true,
+    allowPartial: false,
+    startOn: "human",
+});
 
 // add in a system message with some custom instructions (but still taking messages as input)
 const promptTemplate = ChatPromptTemplate.fromMessages([
     [
       "system",
       "You talk like a master yoda from star wars. Answer all questions to the best of your ability.",
+    ],
+    ["placeholder", "{messages}"],
+]);
+
+const promptTemplate2 = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      "You are an helpful assistant named Jarret. Answer all questions to the best of your ability in {language}.",
+    ],
+    ["placeholder", "{messages}"],
+]);
+
+const promptTemplate3 = ChatPromptTemplate.fromMessages([
+    [
+      "system",
+      "You a Software Engineer interviewer. Ask a quetion about data structures in {language}.",
     ],
     ["placeholder", "{messages}"],
 ]);
@@ -31,13 +69,6 @@ const callModel = async (state: typeof MessagesAnnotation.State) => {
     return { messages };
 };
 
-// Define a new graph
-const workflow = new StateGraph(MessagesAnnotation)
-    // Define the node and edge
-    .addNode("model", callModel)
-    .addEdge(START, "model")
-    .addEdge("model", END);
-
 // Define the function that calls the model
 const callModel2 = async (state: typeof MessagesAnnotation.State) => {
     const llm = new ChatOpenAI({
@@ -50,17 +81,45 @@ const callModel2 = async (state: typeof MessagesAnnotation.State) => {
     return { messages: [response] };
 };
 
+const callModel4 = async (state: typeof GraphAnnotation.State) => {
+    const llm = new ChatOpenAI({
+        model: "gpt-4o-mini",
+        temperature: 0
+    });
+    const trimmedMessage = await trimmer.invoke(state.messages);
+    const prompt = await promptTemplate2.invoke({
+      messages: trimmedMessage,
+      language: state.language,
+    });
+    const response = await llm.invoke(prompt);
+    return { messages: [response] };
+};
+
+// Define a new graph
+const workflow = new StateGraph(MessagesAnnotation)
+    // Define the node and edge
+    .addNode("model", callModel)
+    .addEdge(START, "model")
+    .addEdge("model", END);
+
 // Define a new graph
 const workflow2 = new StateGraph(MessagesAnnotation)
   // Define the (single) node in the graph
   .addNode("model", callModel2)
   .addEdge(START, "model")
   .addEdge("model", END);
-
-  // Add memory
+ 
+const workflow4 = new StateGraph(GraphAnnotation)
+    .addNode("model", callModel4)
+    .addEdge(START, "model")
+    .addEdge("model", END);
+    
+// Add memory
 const memory = new MemorySaver();
 const app = workflow.compile({ checkpointer: memory });
 const app2 = workflow2.compile({ checkpointer: new MemorySaver() });
+const app4 = workflow4.compile({ checkpointer: new MemorySaver() });
+
 const config = { configurable: { thread_id: uuidv4() } };
 
 export const response = async (prompt) => {
@@ -70,9 +129,45 @@ export const response = async (prompt) => {
           content: prompt,
         }
     ];
+    const input2 = {
+        messages: [
+          {
+            role: "user",
+            content: prompt,
+          },
+        ],
+        language: "English",
+    };
+
+    const messages = [
+        new SystemMessage("you're a good assistant"),
+        new HumanMessage("hi! I'm bob"),
+        new AIMessage("hi!"),
+        new HumanMessage("I like vanilla ice cream"),
+        new AIMessage("nice"),
+        new HumanMessage("whats 2 + 2"),
+        new AIMessage("4"),
+        new HumanMessage("thanks"),
+        new AIMessage("no problem!"),
+        new HumanMessage("having fun?"),
+        new AIMessage("yes!"),
+    ];
+      
+    await trimmer.invoke(messages);
+
+    const input4 = {
+        messages: [...messages, new HumanMessage("What is my name?")],
+        language: "English",
+    };
+
+    const input5 = {
+        messages: [...messages, new HumanMessage("What math problem did I ask?")],
+        language: "English",
+      };
 
     // The output contains all messages in the state.
-    const output = await app2.invoke({ messages: input }, config);
+    // const output = await app2.invoke({ messages: input }, config);
+    const output = await app4.invoke(input5, config);
     // This will log the last message in the conversation.
     return output.messages[output.messages.length - 1]
 }
